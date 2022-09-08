5427f38b467e393a9afdffaa233428533b8f2dcf
pchikani <104200638+pchikani@users.noreply.github.com>
2022-09-08 15:27:36 +0530

    Update HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -2,6 +2,7 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        System.out.println("This is auto triggered by commit!");
     }
 }
